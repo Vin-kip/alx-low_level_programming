8a41bf2349495ed9e190b3247b3cabd97bd6e2e8
Vincent <vinkipko@gmail.com>
2022-10-16 01:45:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- *Description: main block
- *main: prints out stdout
- *Return: 0
- */
+*main - main block
+*Return: 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
