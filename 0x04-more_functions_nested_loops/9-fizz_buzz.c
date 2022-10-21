0a68333a551b735b1d0d2459316cf4bf2fdcf5da
Vincent <vinkipko@gmail.com>
2022-10-21 09:14:39 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,7 @@
 
 /**
 * main - Fizz Buzz Test
-* Description: print Fizz for multiples of 3
-* and Buzz for multiples of 5
-* and FizzBuzz for multiples of both
+* Description: print Fizz for multiples of 3 and Buzz for multiples of 5
 * Return: 0 (success)
 */
 
@@ -23,19 +21,14 @@ for (i = 2; i <= 100; i++)
 {
 
 printf(" ");
-
 												if (i % 3 == 0 || i % 5 == 0)
-
 {
-
 if (i % 3 == 0)
-
 												printf("Fizz");
 
 if (i % 5 == 0)
 
 printf("Buzz");
-
 												}
 
 else
