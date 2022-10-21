f35bec415a93256f8e715ce13c0abcead6872cf9
Vincent <vinkipko@gmail.com>
2022-10-21 05:25:24 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,9 @@
 #include "holberton.h"
-
-
-
 /**
- *
- * * main - tests function that prints if integer is positive or negative
- *
- * * Return: 0
- *
- * */
-
-
+*
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+**/
 
 int main(void)
 
@@ -18,14 +11,9 @@ int main(void)
 
 		int i;
 
-
-
 			i = 0;
-
 				positive_or_negative(i);
 
-
-
-					return (0);
+				return (0);
 
 }
