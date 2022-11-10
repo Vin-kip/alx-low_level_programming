4ebaa558e4560eecaf5eee8944788e44a6f4b91a
Vincent <vinkipko@gmail.com>
2022-11-09 23:35:07 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,47 +11,30 @@
 */
 
 char *create_array(unsigned int size, char c)
-
 {
-
 char *str;
-
 unsigned int i;
-
 if (size == 0)
-
 {
 
 return (NULL);
 
 }
-
 else
-
 {
-
 str = malloc(size * sizeof(char));
 
 if (str == NULL)
-
 {
-
 return (NULL);
-
 }
 
 else
-
 {
-
 for (i = 0; i < size; ++i)
 
 str[i] = c;
-
 }
-
 }
-
 return (str);
-
 }
