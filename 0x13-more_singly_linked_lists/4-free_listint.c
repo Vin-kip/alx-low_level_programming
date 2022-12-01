333c63c998fdac3663a9ce72f6b581d3fa1c269c
Vincent <vinkipko@gmail.com>
2022-12-01 01:31:55 -0800

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+#include "lists.h"
+
+/**
+* free_listint - frees a singly linked list.
+* @head: a linked list to print
+* Return: void.
+*/
+
+void free_listint(listint_t *head)
+
+{
+
+listint_t *tmp_node; 
+
+while (head)
+
+{
+
+tmp_node = head;
+head = head->next;
+
+free(tmp_node);
+												}
+
+}
